a42f0f1e505bfaaf5fae7996695c2a1ac6538260
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-19 21:46:59 -0700

    server side proxy works

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use (express.static ('./dist'));
 
 //create axios request that gets data from different server endpoint
 
-app.get ('/artists/:artistID', (req, res) => {
+app.get ('/artists/albums/:artistID', (req, res) => {
   axios
-    .get ('http://localhost:3001/artists/' + req.params.artistID)
+    .get ('http://localhost:3001/artists/albums/' + req.params.artistID)
     .then (function (response) {
       res.send (response.data);
     })
@@ -24,6 +24,17 @@ app.get ('/artists/:artistID', (req, res) => {
 app.get ('/artist/:id', (req, res) => {
   axios
     .get ('http://localhost:3003/artists/' + req.params.id)
+    .then (function (response) {
+      res.send (response.data);
+    })
+    .catch (function (error) {
+      console.log ('error', error);
+    });
+});
+
+app.get ('/artists/:artistID', (req, res) => {
+  axios
+    .get ('http://localhost:3004/artists/' + req.params.artistID)
     .then (function (response) {
       res.send (response.data);
     })
